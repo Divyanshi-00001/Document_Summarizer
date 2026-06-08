6b9444b5f6a56dde6634505c14a11a76d80f8266
240221984@geu.ac.in <divya05ag@gmail.com>
2026-06-09 01:15:36 +0530

    Adding Finalized files

--- preprocess.py
+++ preprocess.py
@@ -1,30 +1,3 @@
-# import nltk
-# from nltk.corpus import stopwords
-# from nltk.tokenize import word_tokenize
-# from nltk.stem import PorterStemmer
-# from nltk.stem import WordNetLemmatizer
-
-# nltk.download('punkt')
-# nltk.download('stopwords')
-# nltk.download('wordnet')
-
-# def preprocess_text(text):
-
-#     tokens = word_tokenize(text)
-
-#     stop_words = set(stopwords.words('english'))
-
-#     tokens = [word for word in tokens
-#               if word.lower() not in stop_words]
-
-#     stemmer = PorterStemmer()
-#     tokens = [stemmer.stem(word) for word in tokens]
-
-#     lemmatizer = WordNetLemmatizer()
-#     tokens = [lemmatizer.lemmatize(word) for word in tokens]
-
-#     return " ".join(tokens)
-
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
